942f2b1cf9b4155b0e308f78904d3b887ef12299
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-09-19 23:22:43 +0700

    Diamond-Gratis

---
+++ diamondfree.sh
@@ -0,0 +1,78 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Diamond
+toilet -f big -F gay Free
+sleep 1
+echo
+echo $cyan"========================================="
+echo $green"    [ Author : Hacker20 ]"
+echo $green"    [ Github : github.com/hacker20 ]"
+echo $cyan"========================================="
+echo
+echo -n $blue"Diamond Apa : ";read M
+
+echo
+sleep 2
+echo $yellow"Sedang Memproses Diamond" $M
+echo
+sleep 2
+echo $green"Proses Diamond $D Selesai"
+echo
+sleep 2
+echo -n $cyan"Nickname = ";read N
+echo
+sleep 2
+echo
+echo -n $cyan"ID = ";read D
+echo
+sleep 2
+echo
+echo -n $red"Berapa Diamond = ";read B
+echo
+sleep 3
+echo
+echo $green"Memperoses Diamond"
+echo
+sleep 3
+echo $yellow"Nickname =" $N
+echo
+sleep 3
+echo $yellow"ID = " $D
+echo
+sleep 3
+echo $yellow"Diamond = " $B
+echo
+sleep 3
+figlet LODAING...
+sleep 4
+echo
+echo $green"Sedang Memproses Diamond Kamu"
+echo
+echo
+sleep 5
+echo $green"PROSES DIAMOND KAMU SELESAI [✓]"
+echo
+sleep 6
+echo $yellow"SEDANG KELUAR DARI TOOLS"
+echo
+echo
+sleep 7
+clear
+figlet B.Y.E
+echo
+sleep 8
+echo $cyan"===================================================="
+echo $green"Diamond Selesai Dikirim Ke ID Dan Nickname Kamu"
+echo $green"Silahkan Tunggu 10 Menit Diamond Nya"
+echo $green"Jangan Lupa Subscribe Channel Gw |Sandi Yt|"
+echo $cyan"===================================================="
+exit
+fi
